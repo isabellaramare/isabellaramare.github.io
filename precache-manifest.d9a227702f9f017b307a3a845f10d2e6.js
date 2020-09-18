f464f03d830d4e53eeb6b117c9607b833e1d4462
isabellaramare <isabella.ramare@hotmail.com>
2020-09-18 14:08:23 +0200

    Updates

---
+++ precache-manifest.d9a227702f9f017b307a3a845f10d2e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed52d72c9d9bff3cea4e58f58c83ec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4273345570c792a1ef72",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2d6245f8b465089abc06",
+    "url": "/static/css/main.7835888b.chunk.css"
+  },
+  {
+    "revision": "4273345570c792a1ef72",
+    "url": "/static/js/2.e95ecf83.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e95ecf83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d6245f8b465089abc06",
+    "url": "/static/js/main.5d53fee0.chunk.js"
+  },
+  {
+    "revision": "802f054ddf00fe69ca5d",
+    "url": "/static/js/runtime-main.3ceab397.js"
+  },
+  {
+    "revision": "702283c4504ceae790ce67ee05142ab5",
+    "url": "/static/media/skull.702283c4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8cf79f7249e8320afdbd51d444f735db.js"
+  "/precache-manifest.d9a227702f9f017b307a3a845f10d2e6.js"
 );
 
 self.addEventListener('message', (event) => {
