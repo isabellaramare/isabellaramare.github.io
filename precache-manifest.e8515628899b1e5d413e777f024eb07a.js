ac922571a39b0b3d12e2665c35f0ad31ba429a28
isabellaramare <isabella.ramare@hotmail.com>
2020-09-18 12:13:05 +0200

    Updates

---
+++ precache-manifest.e8515628899b1e5d413e777f024eb07a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a52b4d91c3ada88a8d631737f54b1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5f154065af01f756210",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "015bba974da808e86e52",
+    "url": "/static/js/2.d6442405.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6442405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f154065af01f756210",
+    "url": "/static/js/main.3c8d33e1.chunk.js"
+  },
+  {
+    "revision": "802f054ddf00fe69ca5d",
+    "url": "/static/js/runtime-main.3ceab397.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
