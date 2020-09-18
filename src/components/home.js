abdd9cea53a12f04d8d1156889cca9642a965ef3
isabellaramare <isabella.ramare@hotmail.com>
2020-09-18 13:06:59 +0200

    trying to add first component

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../images/skull.svg';
 import { Container, Row, Col } from 'react-bootstrap'
-import './App.css';
+import '../App.css';
 
 function Home() {
   return (
