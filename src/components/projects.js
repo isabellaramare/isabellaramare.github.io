e183a1decc2cae0c62d7771b8f9f8ff9f491e9cf
isabellaramare <isabella.ramare@hotmail.com>
2020-10-22 22:51:58 +0200

    working with 'projects'

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,36 +1,92 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import '../App.css';
 import carlitos from '../images/Ricos de Carlitos Color.png'
 import logo from '../images/skull-invert.svg';
 import portrait from '../images/isabella portrait.png'
 
 class Projects extends React.Component { 
-  constructor(props) {
-    super(props);
-    this.state = {
-        currentSlide: 0
-    };
-    this.changeSlide = this.changeSlide.bind(this);
-  }
-
-  handleClick(scrollTo) {
-    var elmnt = document.getElementById(scrollTo);
-    elmnt.scrollIntoView();
-  }
-
-  changeSlide() {
-    var slides = document.querySelectorAll('#slides .slide');
-    var currentSlide = this.state.currentSlide;
-    slides[currentSlide].className = 'slide';
-    var newSlide = (currentSlide + 1)%slides.length;
-    this.setState({
-      currentSlide: newSlide
-    });
-    slides[newSlide].className = 'slide showing';
-  }
-
+  currentSlide = 0
   
+  handleClick(scrollTo) {
+    var element;
+    var slides = document.getElementsByClassName('slide')
+
+   
+
+    if (scrollTo === 'next') {
+    /*   if (this.currentSlide <= 1) {
+        // hide left arrow
+        document.getElementById('btn-previous').style.visibility = 'hidden'
+        return;
+      }
+      else {
+        document.getElementById('btn-previous').style.visibility = 'visible'
+      }
+      this.currentSlide -= 1;      
+      elmnt = document.getElementById(this.currentSlide); */
+
+      // kanske inte behövs om knappen är 'hidden'?
+      // if (this.currentSlide >= (slides.length - 1)) return;
+     
+
+      this.currentSlide += 1
+      element = slides[this.currentSlide]
+      console.log(this.currentSlide)
+    }
+
+    else if (scrollTo === 'previous') {
+      /* if (this.currentSlide >= 3) {
+        document.getElementById('btn-next').style.visibility = 'hidden'
+        return;
+      }
+      else {
+        document.getElementById('btn-next').style.visibility = 'visible'
+      }
+      this.currentSlide += 1;
+      elmnt = document.getElementById(this.currentSlide); */
+
+         // kanske inte behövs om knappen är 'hidden'?
+
+        this.currentSlide -= 1;
+        element = slides[this.currentSlide]
+    }
+
+    else {
+      element = document.getElementById(scrollTo);
+    }
+
+    try {
+      element.scrollIntoView();
+    }
+
+    catch {
+
+    }
+
+
+    switch (this.currentSlide) {
+      case 0: 
+        document.getElementById('btn-previous').style.visibility = 'hidden'
+        document.getElementById('btn-next').style.visibility = 'visible'
+      break;
+
+      case 1: 
+        document.getElementById('btn-next').style.visibility = 'visible'
+        document.getElementById('btn-previous').style.visibility = 'visible'
+        break;
+      
+      case 2: 
+        document.getElementById('btn-next').style.visibility = 'hidden'
+        document.getElementById('btn-previous').style.visibility = 'visible'
+        break;
+
+      default: 
+        document.getElementById('btn-next').style.visibility = 'visible'
+        document.getElementById('btn-previous').style.visibility = 'visible'
+        break;
+    }
+  }
 
   render() {
     return (
@@ -44,78 +100,54 @@ class Projects extends React.Component {
           </Col>        
         </Row> 
         <Row id="artwork" className="justify-content-center vh-min-100 bg-purple">
-          <Col md={11} sm={12} className="my-auto">            
-            <ul id="slides">
-              <li className="slide showing">
-                <Row className="h-100 pl-5">
-                  <Col md={2} className="my-auto pl-5 mr-5 text-pink">
-                    <h1 className="title-lg">illustration & design</h1>
-                  </Col>
-                  <Col md={2} className="my-auto">
-                    <img src={logo} />
-                  </Col>
-                </Row>
-              </li>
-              <li className="slide">
-                <Row className="justify-content-center bg-lavender">
-                  <Col md={6} sm={12} className="my-auto p-5">
+          
+        </Row>      
+        <Row id="code" className="min-vh-100 justify-content-center no-gutters">
+          <Col md={1} sm={0} className="my-auto text-center">
+            <button id="btn-previous" className="btn-slide" style={{visibility: 'hidden'}} onClick={(e) => this.handleClick('previous')}>&lt;</button>
+          </Col>
+          <Col md={10} sm={12} className="pt-5 mt-4 my-md-auto">
+            <div className="slider">          
+              <div className="slides">
+                <Row className="slide no-gutters" id="0">
+                  <Col sm={12} md={6} className="p-sm-0 p-md-5">
                     <img className="w-100" src={carlitos} />
                   </Col>
-                  <Col md={6} sm={12} className="my-auto">
-                    <p>
-                      jrekngj ej fkew wm nnf 4nq4nnk q
+                  <Col sm={12} md={6} className="p-sm-0 p-md-5 my-auto">
+                    <h1 className="text-center">Ricos de Carlitos</h1>
+                    <p className="text-md">
+                      Detta är en logotyp jag skapade åt min bästa väns svärfar som äger en foodtruck i Equador. 
+                      Han säljer en Equadoriansk specialitet: Maduros con Queso, matbananer med ost!
+                      Med hjälp av referensbilder på svärfadern skapade jag denna logotyp i Illustrator. 
+                      Min vän gjorde texten ovan bilden.
                     </p>
                   </Col>
                 </Row>
-              </li>
-              <li className="slide">third slide</li>
-            </ul>
-          </Col>  
-          <Col md={1} sm={0} className="my-auto bg-pink full-width p-0">
-            <button className="btn-next" onClick={this.changeSlide}>
-              <h1 className="title-lg">
-                <strong>
-                  &gt;
-                </strong>
-              </h1>
-            </button>
-          </Col>
-        </Row>      
-        <Row id="code" className="justify-content-center min-vh-100">
-          <Col md={10} sm={12} className="my-auto pt-5">
-            <div className="slider pt-md-5 pb-2">          
-              <div className="slides">
-                <Row className="p-md-5 p-sm-0" id="slide-1">
-                  <Col md={6}>
-                    <img className="w-100 p-md-5 p-sm-0" src={portrait} />
+                <Row className="slide no-gutters" id="1">
+                  <Col sm={12} md={6} className="p-sm-0 p-md-5">
+                    <img className="w-100" src={portrait} />
                   </Col>
-                  <Col md={6}>
-                    <h1>isabella portrait</h1>
+                  <Col sm={12} md={6} className="p-sm-0 p-md-5 my-auto">
+                    <h1 className="text-center">isabella portrait</h1>
+                    <p className="text-md">
+                      isabella portrait isabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portrait</p>                 
                   </Col>
                 </Row>
-                <Row className="p-md-5 p-sm-0" id="slide-2">
-                  <Col md={6}>
-                    <img className="w-100 p-md-5 p-sm-0" src={portrait} />
+                <Row className="slide no-gutters" id="2">
+                  <Col sm={12} md={6} className="p-sm-0 p-md-5">
+                    <img className="w-100" src={logo} />
                   </Col>
-                  <Col md={6}>
-                    <h1>isabella portrait</h1>
-                  </Col>
-                </Row>
-                <Row className="p-md-5 p-sm-0" id="slide-3">
-                  <Col md={6}>
-                    <img className="w-100 p-md-5 p-sm-0" src={logo} />
-                  </Col>
-                  <Col md={6}>
-                    <h1>skull logo</h1>
+                  <Col sm={12} md={6} className="p-sm-0 p-md-5 my-auto">
+                    <h1 className="text-center">isabella portrait</h1>
+                    <p className="text-md">
+                      isabella portrait isabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portraitisabella portrait</p>
                   </Col>
                 </Row>
               </div>
-              <a href="#slide-1">1</a>
-              <a href="#slide-2">2</a>
-              <a href="#slide-3">3</a>
-              <a href="#slide-4">4</a>
-              <a href="#slide-5">5</a>
-            </div>            
+            </div>  
+          </Col>
+          <Col md={1} sm={0} className="my-auto text-center">
+            <button id="btn-next" className="btn-slide" onClick={(e) => this.handleClick('next')}>&gt;</button>          
           </Col>
         </Row>
       </Container>
